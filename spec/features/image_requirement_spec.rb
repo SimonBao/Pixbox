2668b4f8f30ff5cd687930ededbffcf49f3b85e3
Simon Bao <Simonbao.sb@gmail.com>
2018-02-24 15:46:25 +0000

    Updated model to require image

---
+++ spec/features/image_requirement_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper.rb'
+
+feature "Can't create post" do
+  scenario "with no image" do
+    visit '/'
+    click_link 'New Post'
+    fill_in 'Caption', with: 'Hello'
+    click_button 'Create Post'
+    expect(page).to have_content('Halt, you fiend! You need an image to post here!')
+  end
+end
